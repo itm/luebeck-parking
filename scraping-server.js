adceced4eb99fd00fe9907a61f82e9624ef30716
Soenke Nommensen <snommensen@gmail.com>
2011-07-15 11:02:17 +0200

    Jquery himBHZeichen auch im Scraper definiert.

--- scraping-server.js
+++ scraping-server.js
@@ -3,8 +3,9 @@ const request  = require('request'),
       scraper  = require('./scraper'),
       JSON     = require('./json2');
 
-var fetch = function() {
-    console.log("Server started...");
+console.log("Server started...");
+
+var fetch = function() {    
     request({ uri:'http://kwlpls.adiwidjaja.com/index.php' }, function (error, response, body) {
       if (error && response.statusCode !== 200) {
         console.log('Error when contacting google.com')
@@ -16,7 +17,7 @@ var fetch = function() {
           'http://code.jquery.com/jquery-1.6.1.min.js'
         ]
       }, function (err, window) {
-        var $ = window.jQuery;
+        var scraper.scraper.$ = window.jQuery;
     
         scraper.scraper.processPage();
         // jQuery is now loaded on the jsdom window created from 'agent.body'
@@ -25,7 +26,7 @@ var fetch = function() {
   });
 };
 
-const delay = 20000; // 2 minutes  
+const delay = 10000; // 2 minutes  
 var intervalId = setInterval(fetch, delay);
 
 //clearInterval(intervalId);
