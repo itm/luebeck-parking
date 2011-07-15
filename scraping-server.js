65c15cc8fc7c8c4807b4d0e787d20d963c948c6b
Soenke Nommensen <snommensen@gmail.com>
2011-07-15 11:04:55 +0200

    Zuweisungsfehler korrigiert.

--- scraping-server.js
+++ scraping-server.js
@@ -17,7 +17,7 @@ var fetch = function() {
           'http://code.jquery.com/jquery-1.6.1.min.js'
         ]
       }, function (err, window) {
-        var scraper.scraper.$ = window.jQuery;
+        scraper.scraper.$ = window.jQuery;
     
         scraper.scraper.processPage();
         // jQuery is now loaded on the jsdom window created from 'agent.body'
