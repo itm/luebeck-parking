88cf76089d870f7365240c1c575f1b96ea711a32
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-01-19 02:35:02 +0100

    care for orientation change

--- jQuery mobile/js/application.js
+++ jQuery mobile/js/application.js
@@ -1,3 +1,8 @@
+function resizeMap() {
+	//resize map to fit height
+	var mapHeight = $(window).height()-$(".ui-header:first").outerHeight();
+	$('#map-canvas').css('height', mapHeight+'px');
+}
 
 $( document ).delegate("#map", "pageshow", function() {
 
@@ -9,9 +14,10 @@ $( document ).delegate("#map", "pageshow", function() {
 	};
 	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	var infowindow = new google.maps.InfoWindow();
+	resizeMap();
 	
-	//resize map to fit height
-	var mapHeight = $(window).height()-$(".ui-header:first").outerHeight();
-	$('#map-canvas').css('height', mapHeight+'px');
-	
+});
+
+$( document ).bind( "orientationchange", function( event, data ){
+	resizeMap();
 });
