72b2eed5f8a45dc50cf7734a3cdb9bd224589dff
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-01-19 02:30:03 +0100

    built structure including map

---
+++ jQuery mobile/js/application.js
@@ -0,0 +1,17 @@
+
+$( document ).delegate("#map", "pageshow", function() {
+
+	var myOptions = {
+		zoom : 14,
+		center : new google.maps.LatLng(53.890582, 10.701184),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+	var infowindow = new google.maps.InfoWindow();
+	
+	//resize map to fit height
+	var mapHeight = $(window).height()-$(".ui-header:first").outerHeight();
+	$('#map-canvas').css('height', mapHeight+'px');
+	
+});
