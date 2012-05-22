98fc80db0dca3c5a50978dceac4c25a250b40699
Sebastian Ebers <ebers@itm.uni-luebeck.de>
2012-05-22 15:40:13 +0200

    Configuration file added

---
+++ public/mobile/js/config.js
@@ -0,0 +1,4 @@
+
+var datasources = new Array("http://smarthl.itm.uni-luebeck.de/ssp/be-0001/Luebeck",
+    "http://smarthl.itm.uni-luebeck.de/ssp/be-0002/Santander",
+    "http://smarthl.itm.uni-luebeck.de/ssp/be-0002/SantanderParkingSpaces");
